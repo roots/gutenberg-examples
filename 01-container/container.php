5436704eeb8688d6043d331495b211b441434e3c
Kelly Mears <developers@tinypixel.dev>
2020-03-16 19:54:18 -0700

    Fixes, comments, cleans PHP

--- 01-container/container.php
+++ 01-container/container.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name:     Container
  * Description:     Example block written with ESNext standard and JSX support – build step required.
@@ -10,49 +9,36 @@
  */
 
 /**
- * Registers all block assets so that they can be enqueued through the block editor
- * in the corresponding context.
- *
- * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/applying-styles-with-stylesheets/
+ * Registers block assets to be enqueued
+ * @see https://git.io/JvPHi
+ * @see https://git.io/JvPHy
  */
-function create_block_container_block_init() {
-	$dir = dirname( __FILE__ );
+add_action('init', function () {
+    $dir = dirname(__FILE__);
 
-	$script_asset_path = "$dir/build/index.asset.php";
-	if ( ! file_exists( $script_asset_path ) ) {
-		throw new Error(
-			'You need to run `npm start` or `npm run build` for the "create-block/container" block first.'
-		);
-	}
-	$index_js     = 'build/index.js';
-	$script_asset = require( $script_asset_path );
-	wp_register_script(
-		'create-block-container-block-editor',
-		plugins_url( $index_js, __FILE__ ),
-		$script_asset['dependencies'],
-		$script_asset['version']
-	);
+    if (!$manifestPath = realpath("{$dir}/build/index.asset.php")) {
+        new WP_Error(
+            'Missing manifest: "create-block/container"',
+            "Run `npm build` in ${dir}"
+        );
+    }
 
-	$editor_css = 'editor.css';
-	wp_register_style(
-		'create-block-container-block-editor',
-		plugins_url( $editor_css, __FILE__ ),
-		array(),
-		filemtime( "$dir/$editor_css" )
-	);
+    wp_register_script(
+        'create-block-container-block-editor',
+        plugins_url('build/index.js', __FILE__),
+        ...array_values((array) require $manifestPath)
+    );
 
-	$style_css = 'style.css';
-	wp_register_style(
-		'create-block-container-block',
-		plugins_url( $style_css, __FILE__ ),
-		array(),
-		filemtime( "$dir/$style_css" )
-	);
+    foreach (['editor', 'style'] as $asset) {
+        wp_register_style(
+            "create-block-container-block-{$asset}",
+            plugins_url($asset, __FILE__),
+        );
+    }
 
-	register_block_type( 'create-block/container', array(
-		'editor_script' => 'create-block-container-block-editor',
-		'editor_style'  => 'create-block-container-block-editor',
-		'style'         => 'create-block-container-block',
-	) );
-}
-add_action( 'init', 'create_block_container_block_init' );
+    register_block_type('create-block/container', [
+        'editor_script' => 'create-block-container-block-editor',
+        'editor_style' => 'create-block-container-block-editor',
+        'style' => 'create-block-container-block-style',
+    ]);
+});
